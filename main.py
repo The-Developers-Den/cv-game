45ae60e815c42bf565646bbf25a77aad1d081d18
pratham-19 <prathamsinghal2003@gmail.com>
2023-03-21 03:19:49 +0530

    add: Added gameOver functionality

--- main.py
+++ main.py
@@ -18,53 +18,67 @@ class SnakeGame:
         self.currLen = 0
         self.allowedLen = 150
         self.prvHead = 0,0
+        self.score = 0
         self.imgFood = cv2.imread(pathFood,cv2.IMREAD_UNCHANGED)
         self.hFood, self.wFood,_ = self.imgFood.shape
         self.foodPoint = 0,0
         self.randomFoodLoc()
-        self.score = 0
+        self.gameOver = False
     
     def randomFoodLoc(self):
         self.foodPoint = random.randint(0,1000),random.randint(0,600)
 
     def update(self,imgMain,currHead):
-        px,py = self.prvHead
-        cx,cy = currHead
-        self.points.append([cx,cy])
-        dist = math.hypot(cx-px,cy-py)
-        self.lengths.append(dist)
-        self.currLen += dist
-        self.prvHead = currHead
-
-        if self.currLen > self.allowedLen:
-            for i, length in enumerate(self.lengths):
-                self.currLen -= length
-                self.lengths.pop(i)
-                self.points.pop(i)
-                if self.currLen <= self.allowedLen:
-                    break
-
-        
-        rx, ry = self.foodPoint
-        if rx - self.wFood//2 < cx < rx + self.wFood//2 and ry - self.hFood//2 < cy < ry + self.hFood//2:
-            self.randomFoodLoc()
-            self.allowedLen += 40
-            self.score += 1
-
-
-        if (self.points):
-            for i,point in enumerate(self.  points):
-                if i !=0:
-                    cv2.line(imgMain,self.points[i-1],self.points[i],(0,255,0),20)
-            cv2.circle(imgMain, self.points[-1], 10, (255, 0, 255), cv2.FILLED)
-        rx,ry = self.foodPoint
-        # if not (rx - self.wFood//2 < 1280 and ry - self.hFood//2 < 720):
-        #     self.randomFoodLoc()
-        print(rx - (self.wFood//2),ry - (self.hFood//2))
-        if not (rx - (self.wFood//2) > 0 and ry - (self.hFood//2) > 0):
-            self.randomFoodLoc()
+        if self.gameOver:
+            cvzone.putTextRect(imgMain,"Game Over",[300,400],scale=7,thickness=5,font = cv2.FONT_HERSHEY_PLAIN)
+            cvzone.putTextRect(imgMain,f'Score: {self.score}',[300,550],scale=7,thickness=5,font = cv2.FONT_HERSHEY_PLAIN)
+            # self.score = 0
+            
+        else:
+            px,py = self.prvHead
+            cx,cy = currHead
+            self.points.append([cx,cy])
+            dist = math.hypot(cx-px,cy-py)
+            self.lengths.append(dist)
+            self.currLen += dist
+            self.prvHead = currHead
+            if self.currLen > self.allowedLen:
+                for i, length in enumerate(self.lengths):
+                    self.currLen -= length
+                    self.lengths.pop(i)
+                    self.points.pop(i)
+                    if self.currLen <= self.allowedLen:
+                        break            
+            rx, ry = self.foodPoint
+            if rx - self.wFood//2 < cx < rx + self.wFood//2 and ry - self.hFood//2 < cy < ry + self.hFood//2:
+                self.randomFoodLoc()
+                self.allowedLen += 40
+                self.score += 1
+            if (self.points):
+                for i,point in enumerate(self.  points):
+                    if i !=0:
+                        cv2.line(imgMain,self.points[i-1],self.points[i],(0,255,0),20)
+                cv2.circle(imgMain, self.points[-1], 10, (255, 0, 255), cv2.FILLED)
+            
+            if len(self.points) > 2:
+                pts = np.array(self.points[:-2],np.int32)
+                pts.reshape((-1,1,2))
+                cv2.polylines(imgMain,[pts],False,(0,0,255),5)
+                minDist = cv2.pointPolygonTest(pts,currHead,True)
+                if -0.9 <= minDist <= 0.9 :
+                    self.gameOver = True
+                    self.points = []
+                    self.lengths = []
+                    self.currLen = 0
+                    self.allowedLen = 150
+                    self.prvHead = 0,0
+                    cv2.putText(imgMain,"Game Over",(100,100),cv2.FONT_HERSHEY_PLAIN,5,(0,0,255),5)    
             rx,ry = self.foodPoint
-        imgMain = cvzone.overlayPNG(imgMain,self.imgFood,(rx - (self.wFood//2),ry - (self.hFood//2) ))  
+            if not (rx - (self.wFood//2) > 0 and ry - (self.hFood//2) > 0):
+                self.randomFoodLoc()
+                rx,ry = self.foodPoint
+            cvzone.putTextRect(imgMain,f'Score: {self.score}',[50,80],scale=2,thickness=2,font = cv2.FONT_HERSHEY_PLAIN)
+            imgMain = cvzone.overlayPNG(imgMain,self.imgFood,(rx - (self.wFood//2),ry - (self.hFood//2) ))
         return imgMain
 
 game = SnakeGame()
@@ -77,5 +91,9 @@ while True:
         pointIndex = lmList[8][0:2]
         img = game.update(img,pointIndex)
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == ord('x'):
+        break
+    if key == ord('r'):
+        game.gameOver = False
  

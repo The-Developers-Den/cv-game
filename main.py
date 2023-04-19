8272c81ddc8c133d0fd5ec95fc3bbce8fe3bc57b
pratham-19 <prathamsinghal2003@gmail.com>
2023-04-19 14:38:19 +0530

    feat: add bomb

--- main.py
+++ main.py
@@ -11,36 +11,41 @@ pygame.init()
 
 #background
 mixer.music.load('background.wav')
-# mixer.music.play(-1)
+mixer.music.play(-1)
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
+cap.set(3, 1920)
+cap.set(4, 1080)
 
 detector = HandDetector(detectionCon=0.8, maxHands=1)
-
 class SnakeGame:
-    def __init__(self,pathFood="apple.png"):
+    def __init__(self,pathFood="apple.png",pathBomb="bomb1.png"):
         self.points = []
         self.lengths = []
         self.currLen = 0
         self.allowedLen = 150
         self.prvHead = 0,0
         self.score = 0
+        self.finalScore = 0
         self.imgFood = cv2.imread(pathFood,cv2.IMREAD_UNCHANGED)
         self.hFood, self.wFood,_ = self.imgFood.shape
+        self.imgBomb = cv2.imread(pathBomb,cv2.IMREAD_UNCHANGED)
+        self.hBomb, self.wBomb,_ = self.imgBomb.shape
         self.foodPoint = 0,0
+        self.bombPoint = 0,0
         self.randomFoodLoc()
         self.gameOver = False
     
     def randomFoodLoc(self):
         self.foodPoint = random.randint(0,1000),random.randint(0,600)
+    def randomBombLoc(self):
+        self.bombPoint = random.randint(0,1000),random.randint(0,600)
 
     def update(self,imgMain,currHead):
         if self.gameOver:
             cvzone.putTextRect(imgMain,"Game Over",[300,400],scale=7,thickness=5,font = cv2.FONT_HERSHEY_PLAIN)
-            cvzone.putTextRect(imgMain,f'Score: {self.score}',[300,550],scale=7,thickness=5,font = cv2.FONT_HERSHEY_PLAIN)
-            # self.score = 0
+            cvzone.putTextRect(imgMain,f'Score: {self.finalScore}',[300,550],scale=7,thickness=5,font = cv2.FONT_HERSHEY_PLAIN)
+            self.score = 0
             
         else:
             px,py = self.prvHead
@@ -58,12 +63,28 @@ class SnakeGame:
                     if self.currLen <= self.allowedLen:
                         break            
             rx, ry = self.foodPoint
+            bx,by = self.bombPoint
             if rx - self.wFood//2 < cx < rx + self.wFood//2 and ry - self.hFood//2 < cy < ry + self.hFood//2:
                 apple = mixer.Sound('applecrunch.wav')
                 apple.play()
+                self.randomBombLoc()
                 self.randomFoodLoc()
                 self.allowedLen += 60
                 self.score += 1
+            if bx - self.wBomb//2 < cx < bx + self.wBomb//2 and by - self.hBomb//2 < cy < by + self.hBomb//2:
+                self.gameOver = True
+                gmeOver = mixer.Sound('gameover.mp3')
+                gmeOver.play()
+                self.points = []
+                self.lengths = []
+                self.currLen = 0
+                self.allowedLen = 150
+                self.prvHead = 0,0
+                self.finalScore = self.score
+                cv2.putText(imgMain,"Game Over",(100,100),cv2.FONT_HERSHEY_PLAIN,5,(0,0,255),5)   
+                # self.randomFoodLoc()
+                # self.allowedLen += 60
+                # self.score += 1
             if (self.points):
                 for i,point in enumerate(self.  points):
                     if i !=0:
@@ -84,13 +105,19 @@ class SnakeGame:
                     self.currLen = 0
                     self.allowedLen = 150
                     self.prvHead = 0,0
+                    self.finalScore = self.score
                     cv2.putText(imgMain,"Game Over",(100,100),cv2.FONT_HERSHEY_PLAIN,5,(0,0,255),5)    
             rx,ry = self.foodPoint
+            bx,by = self.bombPoint
             while not (rx - (self.wFood//2) > 0 and ry - (self.hFood//2) > 0):
                 self.randomFoodLoc()
                 rx,ry = self.foodPoint
+            while not (bx - (self.wBomb//2) > 0 and ry - (self.hBomb//2) > 0 and bx != rx and by != ry):
+                self.randomBombLoc()
+                bx,by = self.bombPoint
             cvzone.putTextRect(imgMain,f'Score: {self.score}',[50,80],scale=2,thickness=2,font = cv2.FONT_HERSHEY_PLAIN)
             imgMain = cvzone.overlayPNG(imgMain,self.imgFood,(rx - (self.wFood//2),ry - (self.hFood//2) ))
+            imgMain = cvzone.overlayPNG(imgMain,self.imgBomb,(bx - (self.wBomb//2),by - (self.hBomb//2) ))
         return imgMain
 
 game = SnakeGame()
